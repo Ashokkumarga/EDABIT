c430d243192cf7fae7026b7ecf699be17c824981
ashokkumarga <ashokkumarga0@gmail.com>
2022-11-15 13:14:12 +0530

    new up

---
+++ Arrays/return array.js
@@ -0,0 +1,10 @@
+// Return the first Element in an Array.
+
+let arr=["Saab", "Volvo", "BMW"]
+function getFirstValue() {
+    
+	return arr[0]
+}
+console.log(getFirstValue())
+conolr.log(getFirstValue)
+ 
---
+++ BitOperation/booltostring.js
@@ -0,0 +1,7 @@
+let a = true
+
+function boolToString(flag){
+    return flag
+}
+let as= boolToString(a);
+console.log(String(as));
